c6ba17e580f9463ddbdd23459ab65ab2d34567fa
Alfredo Fernández <afernandez@plainconcepts.com>
2015-10-14 16:08:03 +0200

    get port from environment variables

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
   res.sendfile('index.html');
@@ -21,6 +22,6 @@ io.on('connection', function(socket){
   console.log('a user connected');
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(port, function(){
+  console.log('listening on *:' + port);
 });
