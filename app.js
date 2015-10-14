927bec5b3ffe73968d4a79b9948223b1b5685fa0
Alfredo Fernández <afernandez@plainconcepts.com>
2015-10-14 17:52:56 +0200

    testing callback routes for the hooks

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(express.json());
+app.use(express.urlencoded());
+
 app.get('/', function(req, res){
   res.sendfile('index.html');
 });
